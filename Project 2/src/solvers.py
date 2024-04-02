708ce03a726afe239be8e88b553a232ba11e0548
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2024-04-02 12:10:38 +0200

    feat: add LU decomposition

--- Project 2/src/solvers.py
+++ Project 2/src/solvers.py
@@ -10,14 +10,16 @@ class SolverResult:
         self.time = time
         self.finished = finished
         
-
-def solve_jacobi(A: Matrix, b: Matrix, precision: float = 1e-12, error_threshold: float = 1e12) -> SolverResult:
+def validate_matrices(A: Matrix, b: Matrix) -> None:
     if A.shape[0] != A.shape[1]:
         raise ValueError("Matrix A must be square")
     if A.shape[0] != b.shape[0]:
         raise ValueError("Matrix A and vector b must have the same number of rows")
     if b.shape[1] != 1:
         raise ValueError("Vector b must have only one column")
+
+def solve_jacobi(A: Matrix, b: Matrix, precision: float = 1e-12, error_threshold: float = 1e12) -> SolverResult:
+    validate_matrices(A, b)
     
     # https://en.wikipedia.org/wiki/Jacobi_method#Matrix-based_formula
     n = A.shape[0]
@@ -45,12 +47,7 @@ def solve_jacobi(A: Matrix, b: Matrix, precision: float = 1e-12, error_threshold
     return SolverResult(x, error, error_history, iterations, total_time, finished)
 
 def solve_gauss_seidel(A: Matrix, b: Matrix, precision: float = 1e-12, error_threshold: float = 1e12) -> SolverResult:
-    if A.shape[0] != A.shape[1]:
-        raise ValueError("Matrix A must be square")
-    if A.shape[0] != b.shape[0]:
-        raise ValueError("Matrix A and vector b must have the same number of rows")
-    if b.shape[1] != 1:
-        raise ValueError("Vector b must have only one column")
+    validate_matrices(A, b)
     
     # https://en.wikipedia.org/wiki/Gauss%E2%80%93Seidel_method#Element-based_formula
     n = A.shape[0]
@@ -76,3 +73,71 @@ def solve_gauss_seidel(A: Matrix, b: Matrix, precision: float = 1e-12, error_thr
     end_time = time.perf_counter_ns() 
     total_time = (end_time - start_time) / 1e9
     return SolverResult(x, error, error_history, iterations, total_time, finished)
+
+def lu_decomposition(A: Matrix) -> tuple[Matrix, Matrix]:
+    if A.shape[0] != A.shape[1]:
+        raise ValueError("Matrix A must be square")
+    
+    # https://en.wikipedia.org/wiki/LU_decomposition
+    # https://www.geeksforgeeks.org/doolittle-algorithm-lu-decomposition/
+    n = A.shape[0]
+    L = Matrix.new(n, n)
+    U = Matrix.new(n, n)
+    
+    L.set_diagonal(1)
+    for i in range(n):
+        # upper triangular
+        for j in range(i, n):
+            U[i][j] = A[i][j] - sum([L[i][k] * U[k][j] for k in range(i)])
+
+        # lower triangular
+        for j in range(i+1, n):
+            L[j][i] = 1/U[i][i] * (A[j][i] - sum([L[j][k] * U[k][i] for k in range(i)]))
+
+    return L, U
+
+def solve_forward_substitution(L: Matrix, b: Matrix) -> Matrix:
+    validate_matrices(L, b)
+
+    # https://en.wikipedia.org/wiki/Triangular_matrix#Forward_and_back_substitution
+    n = L.shape[0]
+    x = Matrix.vector(n)
+    for m in range(n):
+        x[m][0] = 1/L[m][m] * (b[m][0] - sum([L[m][i] * x[i][0] for i in range(m)]))
+    return x
+
+def solve_backward_substitution(U: Matrix, b: Matrix) -> Matrix:
+    validate_matrices(U, b)
+
+    # https://en.wikipedia.org/wiki/Triangular_matrix#Forward_and_back_substitution
+    # https://algowiki-project.org/en/Backward_substitution
+    n = U.shape[0]
+    x = Matrix.vector(n)
+    for m in range(n-1, -1, -1):
+        x[m][0] = 1/U[m][m] * (b[m][0] - sum([U[m][i] * x[i][0] for i in range(m+1, n)]))
+    return x
+
+
+def solve_lu_decomposition(A: Matrix, b: Matrix) -> SolverResult:
+    validate_matrices(A, b)
+
+    # https://en.wikipedia.org/wiki/LU_decomposition
+    # https://www.sheffield.ac.uk/media/32074/download?attachment
+
+    start_time = time.perf_counter_ns()
+    L, U = lu_decomposition(A)
+    
+    n = A.shape[0]
+    x = Matrix.vector(n)
+    y = Matrix.vector(n)
+    
+    # Ly = b
+    y = solve_forward_substitution(L, b)
+    # Ux = y
+    x = solve_backward_substitution(U, y)
+
+    error = (A * x - b).norm()
+    end_time = time.perf_counter_ns()
+    total_time = (end_time - start_time) / 1e9
+    return SolverResult(x, error, [], 1, total_time, True)
+
--- Project 2/src/solvers_test.py
+++ Project 2/src/solvers_test.py
@@ -19,6 +19,79 @@ def test_jacobi():
     assert result.finished
     assert almost_equal(result.x[0][0], expected[0][0])
     assert almost_equal(result.x[1][0], expected[1][0])
-test_jacobi()
 
+def test_gauss_seidel():
+    A = Matrix([[2, 1],
+                [5, 7]])
+    b = Matrix([[11],
+                [13]])
+
+    result = solve_gauss_seidel(A, b)
+
+    expected = Matrix([[7+1/9], [-3-2/9]])
+    
+    assert result.finished
+    assert almost_equal(result.x[0][0], expected[0][0])
+    assert almost_equal(result.x[1][0], expected[1][0])
+
+def test_lu_decomposition():
+    A = Matrix([[1, 2, 4],
+                [3, 8, 14],
+                [2, 6, 13]])
+    
+    L, U = lu_decomposition(A)
+
+    expected_L = Matrix([[1, 0, 0],
+                         [3, 1, 0],
+                         [2, 1, 1]])
+    expected_U = Matrix([[1, 2, 4],
+                         [0, 2, 2],
+                         [0, 0, 3]])
+    
+    assert L == expected_L
+    assert U == expected_U
+    assert (L * U) == A
+
+def test_forward_substitution():
+    L = Matrix([[1, 0, 0],
+                [3, 1, 0],
+                [2, 1, 1]])
+    b = Matrix([[3],
+                [13],
+                [4]])
+
+    result = solve_forward_substitution(L, b)
+
+    expected = Matrix([[3], [4], [-6]])
+
+    assert result == expected
+
+def test_backward_substitution():
+    U = Matrix([[1, 2, 4],
+                [0, 2, 2],
+                [0, 0, 3]])
+    b = Matrix([[3],
+                [4],
+                [-6]])
+
+    result = solve_backward_substitution(U, b)
+
+    expected = Matrix([[3], [4], [-2]])
+
+    assert result == expected
+
+def test_solve_lu_decomposition():
+    A = Matrix([[1, 2, 4],
+                [3, 8, 14],
+                [2, 6, 13]])
+    b = Matrix([[3],
+                [13],
+                [4]])
+
+    result = solve_lu_decomposition(A, b)
+
+    expected = Matrix([[3], [4], [-2]])
+    
+    assert result.finished
+    assert result.x == expected
 
